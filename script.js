aa2ee45815f8fe948afcb2ce4bf702720752551f
SREEHARI P V <sreehariv07@gmail.com>
2022-08-30 22:18:19 +0530

    added mobile view with functionality

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const share_btn = document.getElementsByClassName("share-btn");
 const share_menu = document.getElementsByClassName("share-menu");
-// console.log(share_menu);
+const share_menu_mobile = document.getElementsByClassName("share-menu-mobile");
 
 share_btn[0].onclick = myfunc;
 
 function myfunc() {
-  console.log("clicked");
   if (share_menu[0].classList.contains("active")) {
     share_menu[0].classList.remove("active");
+    share_menu_mobile[0].classList.remove("active");
   } else {
     share_menu[0].classList.add("active");
+    share_menu_mobile[0].classList.add("active");
   }
 }
